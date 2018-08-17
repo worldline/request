5004593b0a220585793f039ef4506c69052100a4
Yann Odeyer <yann.odeyer@worldline.com>
2018-08-17 16:21:52 +0200

    use timeout for proxy

--- tests/test-proxy-timeout.js
+++ tests/test-proxy-timeout.js
@@ -1,54 +1,36 @@
 'use strict'
 
 var request = require('../index')
-  , url = require('url')
-  , tape = require('tape')
+var tape = require('tape')
 
-var called = false
-  , proxiedHost = 'google.com'
-  , data = ''
+var proxiedHost = 'google.com'
 
-var s = require('net').createServer(function(sock) {
-  called = true
-  sock.once('data', function (c) {
-    data += c
-  })
+var s = require('net').createServer(function (sock) {
+  // Do nothing to simulate a timeout
 })
 
-tape('setup', function(t) {
-  s.listen(0, function() {
+tape('setup', function (t) {
+  s.listen(0, function () {
     s.url = 'http://localhost:' + this.address().port
     t.end()
   })
 })
 
-tape('proxy opts.proxy.timeout', function(t) {
-  request({
-    tunnel: true,
-    url: 'http://' + proxiedHost,
-    proxy: { port: s.address().port, hostname: 'localhost', timeout: 100 }
-  }, function(err, res, body) {
-    t.notEqual(err, null)
-    t.ok(err.code === 'ECONNRESET')
-    t.end()
-  })
-})
-
-tape('proxy opts.timeout', function(t) {
+tape('proxy opts.timeout', function (t) {
   request({
     tunnel: true,
     url: 'http://' + proxiedHost,
     proxy: s.url,
     timeout: 100
-  }, function(err, res, body) {
+  }, function (err, res, body) {
     t.notEqual(err, null)
     t.ok(err.code === 'ECONNRESET')
     t.end()
   })
 })
 
-tape('cleanup', function(t) {
-  s.close(function() {
+tape('cleanup', function (t) {
+  s.close(function () {
     t.end()
   })
 })
